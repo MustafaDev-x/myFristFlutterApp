23b2de9548871f7b851508a765e47eb67945c18f
muostafa <39297860+muostafa@users.noreply.github.com>
2019-02-24 20:23:02 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'Pages/page1.dart';
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random(); // Add this line.
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(    // Change "const" to "new".
+          //child: const Text('Hello World'),   // Replace this text...
+          child: new Text(wordPair.asPascalCase),  // With this text.
+        ),
+      ),
+    );
+  }
+}
